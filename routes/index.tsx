3cf9594083241cad9b3234427d7dcc70a287fa9a
devoluty <mariotqm1@gmail.com>
2023-02-18 14:23:53 -0600

    improve code

--- routes/index.tsx
+++ routes/index.tsx
@@ -11,19 +11,19 @@ export const handler: Handlers = {
 
 export default function Home(props: PageProps) {
   const { data } = props;
-  const { posts } = data;
+  const { posts } = data ?? {};
 
   return (
     <div class="p-4">
       <h1 class="text-gray-900 text-3xl">Deno Blog</h1>
       <div class="py-5">
-        {posts.map((post: Post) => (
-          <article>
+        {posts.map(({ id, title, date }: Post) => (
+          <article key={id}>
             <h2 class="font-bold text-lg hover:text-blue-900">
-              <a href={`/blog/${post.id}`}>{post.title}</a>
+              <a href={`/blog/${id}`}>{title}</a>
             </h2>
             <span class="text-gray-600 text-sm">
-              {Intl.DateTimeFormat("es").format(post.date)}
+              {Intl.DateTimeFormat("es").format(date)}
             </span>
           </article>
         ))}
--- utils/posts.ts
+++ utils/posts.ts
@@ -3,49 +3,37 @@ import { extract } from "$std/encoding/front_matter/any.ts";
 import { render } from "https://deno.land/x/gfm@0.2.0/mod.ts";
 
 export async function loadPost(id: string): Promise<Post | null> {
-  let raw: string;
   try {
-    raw = await Deno.readTextFile(`./content/posts/${id}.md`);
-  } catch {
+    const raw = await Deno.readTextFile(`./content/posts/${id}.md`);
+    const { attrs, body } = extract(raw);
+    const params = attrs as Record<string, string>;
+
+    const post: Post = {
+      id,
+      title: params.title,
+      body: render(body),
+      date: new Date(params.date),
+      excerpt: params.excerpt,
+    };
+
+    return post;
+  } catch (e) {
+    console.error(`Error loading post ${id}: ${e.message}`);
     return null;
   }
-
-  const { attrs, body } = extract(raw);
-  const params = attrs as Record<string, string>;
-
-  const post: Post = {
-    id,
-    title: params.title,
-    body: render(body),
-    date: new Date(params.date),
-    excerpt: params.excerpt,
-  };
-
-  return post;
 }
 
-export async function listPostsSquential(): Promise<Post[]> {
-  const posts = [];
-  for await (const entry of Deno.readDir("./content/posts")) {
-    const { name } = entry;
-    const [id] = name.split(".");
-    const post = await loadPost(id);
-    if (!post) continue;
-    posts.push(post);
-  }
-
-  console.log(posts);
-  return posts;
-}
 export async function listPosts(): Promise<Post[]> {
-  const promises = [];
+  const promises: Promise<Post | null>[] = [];
   for await (const entry of Deno.readDir("./content/posts")) {
     const { name } = entry;
     const [id] = name.split(".");
     if (id) promises.push(loadPost(id));
   }
 
-  const posts = await Promise.all(promises) as Post[];
+  const posts = (await Promise.all(promises)).filter((p): p is Post =>
+    p !== null
+  );
 
   posts.sort((a, b) => {
     return b.date.getTime() - a.date.getTime();
@@ -53,5 +41,3 @@ export async function listPosts(): Promise<Post[]> {
 
   return posts;
 }
-
-await listPosts();
